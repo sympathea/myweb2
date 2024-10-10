8a61ceba8027d6c8a41d8d39a021cccd0fb27255
Zephyr Lin <lzh910954163@gmail.com>
2024-10-10 00:04:54 -0400

    /blog: blog page initial setup and styling

--- components/Blogs.jsx
+++ components/Blogs.jsx
@@ -1,43 +1,41 @@
-import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import MotionWrapper from "./MotionWrapper";
 
 export default function Blogs({ blogs }) {
   return (
     <section>
-      <ul className="grid w-full grid-cols-1 gap-10 mx-auto sm:grid-cols-2 md:grid-cols-3">
-        {blogs.map((project) => (
-          <motion.div
-            key={project._id}
+      <ul className="grid w-full grid-cols-1 gap-10 mx-auto md:grid-cols-2">
+        {blogs.map((blog) => (
+          <MotionWrapper
+            key={blog._id}
             initial={{ y: 30 }}
             animate={{ y: 0 }}
             whileHover={{
               scale: 1.02,
             }}
             transition={{ type: "spring", stiffness: 200 }}
-            className="relative flex flex-col items-start justify-center gap-6 p-5 shadow-sm rounded-2xl hover:bg-opacity-80 hover:bg-gray-100 hover:border hover:border-dashed hover:border-slate-400"
+            className="relative border shadow-sm rounded-2xl hover:bg-opacity-80 hover:bg-gray-100 hover:border-dashed hover:border-slate-400"
           >
-            <div className="relative flex items-center justify-center w-12 h-12 border rounded-full ">
-              <Image
-                src={project.imageUrl}
-                alt="Project image"
-                width={36}
-                height={36}
-                className="object-contain rounded-full"
-              />
-            </div>
+            <Link href={`/blog/${blog.slug}`}>
+              <div
+                href={`/blog/${blog.slug}`}
+                className="relative aspect-[240/135] w-full"
+              >
+                <Image
+                  src={blog.image}
+                  alt="Blog image"
+                  fill
+                  className="object-cover rounded-lg"
+                />
+              </div>
 
-            <div className="">
-              <h2 className="mb-4 font-bold">{project.title}</h2>
-              <p className="text-sm text-muted-foreground">
-                {project.description}
-              </p>
-            </div>
-
-            <Link href={project.link} target="_blank">
-              <span className="absolute -inset-x-0 -inset-y-0 sm:rounded-2xl" />
+              <div className="absolute bottom-0 left-0 right-0 p-2 bg-black rounded-lg bg-opacity-30 backdrop-blur-sm text-background">
+                <h2 className="mb-4 font-bold">{blog.title}</h2>
+                <p className="text-sm ">{blog.summary}</p>
+              </div>
             </Link>
-          </motion.div>
+          </MotionWrapper>
         ))}
       </ul>
     </section>
