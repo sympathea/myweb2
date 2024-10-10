596bea39cd6675e9c9171a4a49990b384d1f4687
Zephyr Lin <lzh910954163@gmail.com>
2024-10-10 00:01:18 -0400

    /blog: move code that fetch data to lib folder

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,16 +1,18 @@
 import Blogs from "@/components/Blogs";
 import Description from "@/components/Description";
-import Link from "next/link";
+import { getBlogs } from "@/lib/blog";
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  const blogs = await getBlogs();
+
   return (
-    <div className="px-4 mx-auto mt-16 max-w-7xl md:px-8">
+    <div className="flex flex-col gap-20 px-4 mx-auto mt-16 max-w-7xl md:px-8 ">
       <Description
         page="Blog"
         description="Welcome to my blog! I will post some articles about my life, my work, and my thoughts here."
       />
-      {/* <Blogs /> */}
-      <Link href={"/blog/introduction-to-mdx"}>To</Link>
+
+      <Blogs blogs={blogs} />
     </div>
   );
 }
