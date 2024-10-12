12328ccc919c3718176dd39589353606b77b1409
Zephyr Lin <lzh910954163@gmail.com>
2024-10-12 15:04:12 -0400

    Home page initial setup

--- app/page.js
+++ app/page.js
@@ -1,3 +1,14 @@
 export default function Page() {
-  return <div className="mx-auto max-w-7xl"></div>;
+  return (
+    <div className="flex items-center justify-center mx-auto max-w-7xl h-[calc(100vh-10rem)]">
+      <h1 className="text-4xl font-semibold leading-10">
+        Hi, I'm
+        <br />
+        zephyr lin ✌️
+        <br />
+        A Web &lt;Developer/&gt; .
+        <br />
+      </h1>
+    </div>
+  );
 }
