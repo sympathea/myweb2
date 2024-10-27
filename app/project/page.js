59610553e62f0765dbbe4fdc219dd8541dd6ac12
Zephyr Lin <lzh910954163@gmail.com>
2024-10-27 14:01:57 -0400

    /project: remove caching projects

--- app/project/page.js
+++ app/project/page.js
@@ -5,7 +5,7 @@ import { revalidatePath } from "next/cache";
 import Description from "@/components/Description";
 
 export default async function ProjectPage() {
-  revalidatePath("/project");
+  // revalidatePath("/project");
   const query = `*[_type == 'project'] | order(_updatedAt desc) {
         title,
           _id,
@@ -15,13 +15,13 @@ export default async function ProjectPage() {
           "imageUrl": image.asset->url
     }`;
 
-  const projects = await client.fetch(query, { cache: "no-cache" });
+  const projects = await client.fetch(query);
 
   return (
     <div className="flex flex-col gap-20 mt-16 ">
       <Description
         page="Project"
-        description="Showcasing my exploration and experimentation in the tech field. Progressing...."
+        description="Showcase my exploration and experimentation in the computer science field. Progressing...."
       />
       <Projects projects={projects} />
     </div>
