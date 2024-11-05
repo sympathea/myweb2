78e2a273fee53e9c5dab429eb376183320e2c479
Zephyr Lin <lzh910954163@gmail.com>
2024-11-05 12:55:38 -0500

    /: adding gradient to hero and make it bigger

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import Socials from "./Socials";
 export default function Hero() {
   return (
     <>
-      <div className="text-[42px] font-semibold mb-4">
+      <div className="text-[46px] font-semibold mb-4">
         <span>{"<Developer/>,"}</span>
         <span className="relative inline-block px-2 py-1 border border-dashed bg-muted">
           {"Explorer"}
@@ -16,14 +16,14 @@ export default function Hero() {
 
       <p className="flex gap-3 mb-8 text-2xl font-normal tracking-tight text-green-200 opacity-60">
         <span>#Zephyr</span>
-        <span>#智昊</span>
         <span>#Zhihao</span>
+        <span>#智昊</span>
       </p>
 
-      <p className="text-[16px] text-muted-foreground mb-4">
-        I am an undergraduate student at University of Minnesota Twin Cities
-        majoring in Computer Science. I am passionate about developing
-        applications that merge aesthetics with purpose.
+      <p className="mb-4 text-transparent bg-gradient-to-b to-muted-foreground to-70% from-foreground bg-clip-text">
+        Hi, I&apos;m Zephyr, an undergraduate student at University of Minnesota
+        Twin Cities majoring in Computer Science. I&apos;m passionate about
+        developing applications that merge aesthetics with purpose.
       </p>
 
       <Socials />
