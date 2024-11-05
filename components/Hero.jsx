593494bb19ea9c37f942e845ca7616354305cbb8
Zephyr Lin <lzh910954163@gmail.com>
2024-11-05 13:15:27 -0500

    /: hero h1 gradient

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,9 +3,11 @@ import Socials from "./Socials";
 export default function Hero() {
   return (
     <>
-      <div className="text-[46px] font-semibold mb-4">
-        <span>{"<Developer/>,"}</span>
-        <span className="relative inline-block px-2 py-1 border border-dashed bg-muted">
+      <div className="lg:text-[46px] font-semibold mb-4 text-[40px]">
+        <span className="text-transparent sm:bg-gradient-to-l to-muted-foreground bg-gradient-to-t to-70% from-foreground bg-clip-text">
+          {"<Developer/>,"}
+        </span>
+        <span className="relative inline-block px-2 py-1 text-transparent border border-dashed bg-muted sm:bg-gradient-to-r bg-gradient-to-b to-muted-foreground from-foreground bg-clip-text">
           {"Explorer"}
           <div className="absolute w-2 h-2 border border-dashed bg-background border-muted-foreground -top-1 -left-1"></div>
           <div className="absolute w-2 h-2 -top-1 -right-1 bg-muted-foreground"></div>
@@ -14,13 +16,13 @@ export default function Hero() {
         </span>
       </div>
 
-      <p className="flex gap-3 mb-8 text-2xl font-normal tracking-tight text-green-200 opacity-60">
+      <p className="flex gap-3 mb-8 text-xl font-normal tracking-tight text-green-200 sm:text-2xl opacity-60">
         <span>#Zephyr</span>
         <span>#Zhihao</span>
         <span>#智昊</span>
       </p>
 
-      <p className="mb-4 text-transparent bg-gradient-to-b to-muted-foreground to-70% from-foreground bg-clip-text">
+      <p className="mb-4 text-sm sm:text-base text-transparent bg-gradient-to-b to-muted-foreground to-70% from-foreground bg-clip-text">
         Hi, I&apos;m Zephyr, an undergraduate student at University of Minnesota
         Twin Cities majoring in Computer Science. I&apos;m passionate about
         developing applications that merge aesthetics with purpose.
