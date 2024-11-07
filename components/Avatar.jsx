44ee71520f160ea175bff90df64e84006101c57e
Zephyr Lin <lzh910954163@gmail.com>
2024-11-06 22:55:17 -0500

    change avatar

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -7,13 +7,13 @@ import avatarImg from "@/public/images/home/avatar.png";
 
 export default function Avatar({ page }) {
   return (
-    <div className="flex items-end justify-start col-span-1">
+    <div className="flex items-center justify-start col-span-1">
       <Link href="/">
-        {/* <div className="hidden text-2xl font-semibold rounded-full opacity-50 sm:flex sm:justify-center sm:items-center hover:opacity-90 ">
-            ZL
-          </div> */}
+        <div className="items-center justify-center text-3xl font-bold opacity-50 hover:opacity-90">
+          ZL
+        </div>
 
-        <div className="flex flex-col items-start justify-end">
+        {/* <div className="flex flex-col items-start justify-end">
           <Image
             src={avatarImg}
             width={60}
@@ -21,12 +21,8 @@ export default function Avatar({ page }) {
             className={`duration-1000 rounded-full opacity-60 hover:opacity-90 ${page == "/" ? "scale-150 translate-y-20 " : ""}`}
             unoptimized
           />
-        </div>
+        </div> */}
       </Link>
-
-      {/* <div className="sm:hidden">
-        <MobileMenu />
-      </div> */}
     </div>
   );
 }
