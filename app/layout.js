c76a0efdb05be34c4b375d704b74bac44d2426ae
Zephyr Lin <lzh910954163@gmail.com>
2024-11-22 18:21:40 -0500

    feat: add sonner

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import MainWrapper from "@/components/MainWrapper";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Toaster } from "sonner";
 
 const roboto_mono = Roboto_Mono({ subsets: ["latin"] });
 
@@ -23,13 +24,14 @@ export default function RootLayout({ children }) {
     >
       <ClerkProviderWrapper>
         <body className={`${roboto_mono.className} bg-bg-image `}>
-          <div className="flex flex-col items-center max-w-4xl px-4 pt-10 mx-auto lg:max-w-5xl sm:px-12 md:px-20 lg:px-12 xl:max-w-7xl min-h-svh ">
+          <div className="flex flex-col items-center px-4 pt-10 mx-auto max-w-4xl lg:max-w-5xl sm:px-12 md:px-20 lg:px-12 xl:max-w-7xl min-h-svh">
             <Header />
             <MainWrapper>{children}</MainWrapper>
             <Footer />
           </div>
           <Analytics />
           <SpeedInsights />
+          <Toaster />
         </body>
       </ClerkProviderWrapper>
     </html>
